7536c9a66f60316187f24dca374228596248f4c7
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-08 12:17:10 +0100

    Time: 1 ms (100.00%), Space: 39.2 MB (77.63%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,18 +8,60 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         ListNode result = new ListNode();
+//         ListNode current = result;
+//         int index = 0;
+//         int quote = 0;
+        
+//         while (l1 != null || l2 != null) {
+//             if (index != 0) {
+//                 current.next = new ListNode();
+//                 current = current.next;
+//             }
+            
+//             int val1 = 0;
+//             if (l1 != null) {
+//                 val1 = l1.val;
+//                 l1 = l1.next;
+//             }
+            
+//             int val2 = 0;
+//             if (l2 != null) {
+//                 val2 = l2.val;
+//                 l2 = l2.next;
+//             }
+            
+//             int value = val1 + val2+quote;
+//             if (value >= 10) {
+//                 quote = 1;
+//                 value = value%10;
+//             } else {
+//                 quote = 0;
+//             }
+//             current.val = value;
+//             index++;
+//         }
+        
+//         if (quote == 1) {
+//             current.next = new ListNode(1);
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        ListNode current = result;
-        int index = 0;
-        int quote = 0;
+        ListNode fakeResult = new ListNode();
+        ListNode current = fakeResult;
+        int carry = 0;
         
         while (l1 != null || l2 != null) {
-            if (index != 0) {
-                current.next = new ListNode();
-                current = current.next;
-            }
+            current.next = new ListNode();
+            current = current.next;
             
             int val1 = 0;
             if (l1 != null) {
@@ -33,20 +75,15 @@ class Solution {
                 l2 = l2.next;
             }
             
-            int value = val1 + val2+quote;
-            if (value >= 10) {
-                quote = 1;
-                value = value%10;
-            } else {
-                quote = 0;
-            }
+            int value = val1 + val2+carry;
+            carry = value/10;
+            value = value%10;
             current.val = value;
-            index++;
         }
         
-        if (quote == 1) {
+        if (carry == 1) {
             current.next = new ListNode(1);
         }
-        return result;
+        return fakeResult.next;
     }
 }
