112dab5fd2bf7ef6c863aa8d160175d94f6cd8b3
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-09 12:24:49 +0100

    Time: 22 ms (21.62%), Space: 43.3 MB (8.38%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,18 +1,42 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int max = 0;
+//         char[] chars = s.toCharArray();
+//         for (int i=0; i<chars.length;i++) {
+//             Set substring = new HashSet<Character> ();
+//             substring.add(chars[i]);
+//             for (int j=i+1;j<chars.length;j++) {
+//                 if (substring.contains(chars[j])) {
+//                     break;
+//                 }
+//                 substring.add(chars[j]);
+//             }
+//             int temp = substring.size();
+//             max = max>temp ? max : temp;
+//         }
+//         return max;
+//     }
+// }
+
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
+        int start = 0;
+        int end = 0;
+        int i = 0;
         char[] chars = s.toCharArray();
-        for (int i=0; i<chars.length;i++) {
-            Set substring = new HashSet<Character> ();
-            substring.add(chars[i]);
-            for (int j=i+1;j<chars.length;j++) {
-                if (substring.contains(chars[j])) {
-                    break;
-                }
-                substring.add(chars[j]);
+        Set<Character> substring = new HashSet<Character> ();
+        
+        while (end != chars.length) {
+            if (substring.contains(chars[end])) {
+                substring.remove(chars[start]);
+                start += 1;
+            } else {
+                substring.add(chars[end]);
+                end += 1;
+                max = max>substring.size() ? max : substring.size();
             }
-            int temp = substring.size();
-            max = max>temp ? max : temp;
         }
         return max;
     }
