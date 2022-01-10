d32f7c56708ea20dbcf97f3c4903809693f3237c
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-10 15:39:12 +0100

    Time: 33 ms (59.74%), Space: 39.4 MB (62.93%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        String result = String.copyValueOf(chars, 0, 1);
+        int max = 1;
+        for (int i=0; i<chars.length; i++) {
+            int j=1;
+            // for odd palindrome
+            while (i-j >=0 && i+j < chars.length) {
+                if (chars[i-j] == chars[i+j]) {
+                    if (max < 2*j + 1) {
+                        result = String.copyValueOf(chars, i-j, 2*j+1);
+                        max = 2*j+1;
+                    } 
+                } else {
+                    break;
+                }
+                j++;
+            }
+            
+            //for even palindrome
+            j=0;
+            if (i-1 >=0 && chars[i-1] == chars[i]) {
+                while (i-j-1 >= 0 && i + j < chars.length) {
+                    if (chars[i-j-1] == chars[i+j]) {
+                    if (max < 2*j + 2) {
+                        result = String.copyValueOf(chars, i-j-1, 2*j+2);
+                        max = 2*j+2;
+                    } 
+                } else {
+                    break;
+                }
+                j++;
+                }
+            }
+            
+            j=0;
+            if (i+1 <chars.length && chars[i+1] == chars[i]) {
+                while (i-j >= 0 && i + j+1 < chars.length) {
+                    if (chars[i-j] == chars[i+j+1]) {
+                    if (max < 2*j + 2) {
+                        result = String.copyValueOf(chars, i-j, 2*j+2);
+                        max = 2*j+2;
+                    } 
+                } else {
+                    break;
+                }
+                j++;
+                }
+            }
+        }
+        
+        return result;
+        
+    }
+}
