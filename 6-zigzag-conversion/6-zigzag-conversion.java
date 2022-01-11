2cf00c1729077c3de3bc9da71edfc5db7f2e38a4
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-11 18:15:54 +0100

    Time: 8 ms (56.21%), Space: 39.8 MB (57.38%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,37 +1,71 @@
+// class Solution {
+//     public String convert(String s, int numRows) {
+//         if (numRows == 1) {
+//             return s;
+//         }
+//         char[] s_array = s.toCharArray();
+//         String result = verticesChars(s_array, numRows, 0);
+//         for (int i=1; i<numRows -1; i++) {
+//             result += midChars(s_array, numRows, i);
+//         }
+//         return result+verticesChars(s_array, numRows, numRows-1);
+//     }
+    
+//     public String verticesChars(char[] s, int numRows, int start) {
+//         String result = "";
+//         while (start < s.length) {
+//             result += s[start];
+//             start += 2*numRows-2;
+//         }
+//         return result;
+//     }
+    
+//     public String midChars(char[] s, int numRows, int row) {
+//         String result = "";
+//         int i = row;
+//         while (i < s.length){
+//             result += s[i];
+//             i += 2*numRows -2*row-2;
+//             if (i >= s.length){
+//                 break;
+//             }
+//             result += s[i];
+//             i += 2*row;
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
+        if(numRows == 1) {
             return s;
         }
-        char[] s_array = s.toCharArray();
-        String result = verticesChars(s_array, numRows, 0);
-        for (int i=1; i<numRows -1; i++) {
-            result += midChars(s_array, numRows, i);
+        
+        StringBuilder[] rows = new StringBuilder[numRows];
+        char[] sArray = s.toCharArray();
+        int rowPointer = 0;
+        boolean downflag = true;
+        
+        for(int i=0;i<numRows;i++){
+            rows[i]=new StringBuilder("");  
         }
-        return result+verticesChars(s_array, numRows, numRows-1);
-    }
-    
-    public String verticesChars(char[] s, int numRows, int start) {
-        String result = "";
-        while (start < s.length) {
-            result += s[start];
-            start += 2*numRows-2;
-        }
-        return result;
-    }
-    
-    public String midChars(char[] s, int numRows, int row) {
-        String result = "";
-        int i = row;
-        while (i < s.length){
-            result += s[i];
-            i += 2*numRows -2*row-2;
-            if (i >= s.length){
-                break;
+        
+        for (int i=0; i<sArray.length; i++) {
+            if (downflag) {
+                rows[rowPointer++].append(sArray[i]);
+                downflag = rowPointer == numRows-1 ? false : true; 
+            } else {
+                rows[rowPointer--].append(sArray[i]);
+                downflag = rowPointer == 0 ? true : false;
             }
-            result += s[i];
-            i += 2*row;
+        }
+        
+        String result = "";
+        for (int i=0; i<numRows; i++) {
+            result += rows[i];
         }
         return result;
     }
+    
 }
