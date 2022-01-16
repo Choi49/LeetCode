40e9709eb06545ee7109c868c835d677f848484b
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-16 23:34:55 +0100

    Time: 6 ms (99.96%), Space: 38.4 MB (72.49%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) {
+            return false;
+        }
+        int tmp = x;
+        int reverse = 0;
+        while (x!=0) {
+            reverse = 10*reverse + x%10;
+            x=x/10;
+        }
+        return tmp == reverse;
+        
+    }
+}
