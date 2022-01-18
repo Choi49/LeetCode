6dc3183a78b4b9fbb6c929aa6ffa20ac605a06f1
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-01-18 18:02:01 +0100

    Time: 7 ms (57.15%), Space: 41.8 MB (40.85%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    char romans[] = new char[] {'I','V','X','L','C','D','M'};
+    int values[] = new int[] {1,5,10,50,100,500,1000};
+    
+    public String intToRoman(int num) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 6; i >= 0; i--){
+            num = intToRomanChar(i, num, result);
+        }
+        
+        return result.toString();
+    }
+    
+    public int intToRomanChar(int i, int num, StringBuilder result) {
+        int symbol_value = values[i];
+        
+        
+        
+        char c = romans[i];
+        char pre_c = '\0';
+        int pre_symbol_value = 0;
+        
+        int count = 0;
+        
+        if(i >= 1) {
+            int pre = (i + i%2)-2;
+            pre_c = romans[pre];
+            pre_symbol_value = values[pre];
+        }
+        
+        if (num < symbol_value - pre_symbol_value) return num;
+        
+        
+        int quote = num / symbol_value;
+        for (int j = 0; j < quote ; j++) {
+            result.append(c);
+        }
+        
+        num = num % symbol_value;
+        if (num >= symbol_value - pre_symbol_value) {
+            num -= symbol_value - pre_symbol_value;
+            result.append(pre_c);
+            result.append(c);
+        }
+        
+        return num;
+    }
+}
