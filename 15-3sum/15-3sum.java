e8c3ceaad22e1afe4b7257c179638f327c5ac743
Choi49 <57182676+Choi49@users.noreply.github.com>
2022-02-17 16:19:54 +0100

    Time: 44 ms (38.66%), Space: 58.5 MB (36.87%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,41 +1,5 @@
 import java.util.Arrays;
 
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-//         if(nums.length < 3){
-//             return new ArrayList<>();
-//         }
-//         Arrays.sort(nums);
-//         int max = nums[nums.length-1];
-//         List<List<Integer>> result = new ArrayList<>();
-//         for (int i =0; i<nums.length-2; i++) {
-//             for (int j=i+1; j<nums.length-1; j++) {
-//                 if (nums[i]+nums[j] > nums[j]) {
-//                     break;
-//                 } 
-//                 for (int k = j+1; k<nums.length; k++) {
-//                     if (nums[i] + nums[j] + nums[k]==0) {
-//                         List triplet = List.of(nums[i],nums[j],nums[k]);
-//                         boolean check = true;
-//                         for (List l : result){
-//                             if (Arrays.equals(l.toArray(), triplet.toArray())) {
-//                                 check = false;
-//                                 break;
-//                             }
-//                         }
-//                         if (check) {
-//                             result.add(List.of(nums[i],nums[j],nums[k]));
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return result;
-//     }
-// }
-
-
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         if(nums.length < 3){
